cdb34110732d1b98faeda3de96918dc1ba824a9f
Emanuel López <emanuel.n.lopez@gmail.com>
2024-10-04 15:20:11 -0300

    Adds swipe gesture to delete the entries

--- flutter_journal_app/lib/src/widgets/entry_list_item.dart
+++ flutter_journal_app/lib/src/widgets/entry_list_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_journal_app/src/ui/ui.dart';
 import 'package:flutter_journal_app/src/utils/utils.dart';
 import 'package:flutter_journal_presentation/flutter_journal_presentation.dart';
+import 'package:provider/provider.dart';
 
 class EntryListItem extends StatelessWidget {
   const EntryListItem({
@@ -13,32 +14,46 @@ class EntryListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: Spacing.small),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(entry.text),
-          if (entry.tags.isNotEmpty)
-            Row(
-              children: entry.tags
-                  .map((tag) => Chip(
-                        padding: const EdgeInsets.all(Spacing.xsmall),
-                        label: Text(tag),
-                      ))
-                  .toList(),
-            ),
-          SizedBox(
-            width: double.infinity,
-            child: Text(
-              DateTimeUtils.geDateFormat(
-                DateTimeUtils.fullDateTimeFormat,
-              ).format(entry.dateTime),
-              textAlign: TextAlign.right,
-            ),
+    return Dismissible(
+      background: const ColoredBox(
+        color: Colors.red,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: Spacing.medium),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [Icon(Icons.delete), Icon(Icons.delete)],
           ),
-        ],
+        ),
+      ),
+      key: ValueKey(entry.id),
+      onDismissed: (direction) => context.read<EntryController>().delete(entry),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: Spacing.small),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(entry.text),
+            if (entry.tags.isNotEmpty)
+              Row(
+                children: entry.tags
+                    .map((tag) => Chip(
+                          padding: const EdgeInsets.all(Spacing.xsmall),
+                          label: Text(tag),
+                        ))
+                    .toList(),
+              ),
+            SizedBox(
+              width: double.infinity,
+              child: Text(
+                DateTimeUtils.geDateFormat(
+                  DateTimeUtils.fullDateTimeFormat,
+                ).format(entry.dateTime),
+                textAlign: TextAlign.right,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
